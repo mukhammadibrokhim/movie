25dc92201f2e3815e0079e9eb79ebd2185c97a49
Mukhammadibrokhim <tursunboyev06061999@gmail.com>
2024-04-27 22:14:35 +0500

    hhh

--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="app">
-  <div class="content">
-    <AppInfo />
-  </div>
+    <div class="content">
+      <AppInfo />
+    </div>
+    <div class="search-panel">
+      
+    </div>
   </div>
  
 </template>
@@ -27,5 +30,13 @@ import  AppInfo from '@/components/app-info/AppInfo.vue'
   background-color: #fff;
   margin: 0 auto;
   padding: 5rem 0
+}
+
+.search-panel {
+  margin-top: 2rem;
+  padding: 1.5rem;
+  background-color: #fcfaf5;
+  border-radius: 4px;
+  box-shadow: 15px, 15px, 15px rgba(0, 0, 0, 0.15);
 }
 </style>
