ef80bbf1f7a7399912d86024efc137cee031a577
Mukhammadibrokhim <tursunboyev06061999@gmail.com>
2024-04-27 21:57:31 +0500

    This is test vue js movie app

---
+++ src/components/app-info/AppInfo.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Appinfo</h1>
+</template>
+
+<script>
+ export default {
+    
+ }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/app/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app">
+  <div class="content">
+    <AppInfo />
+  </div>
+  </div>
+ 
+</template>
+
+<script>
+import  AppInfo from '@/components/app-info/AppInfo.vue'
+  export default {
+    components: {
+      AppInfo,
+    }
+  }
+</script>
+
+<style>
+  .app {
+    height: 100vh;
+    color: black;
+  }
+.content {
+  width: 1000px;
+  min-height: 700px;
+  background-color: #fff;
+  margin: 0 auto;
+  padding: 5rem 0
+}
+</style>
